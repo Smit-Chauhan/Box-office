135a0cee6cecdeb3e2866c3a82128d6d215702e2
devanshichauhan54 <98576336+Smit-Chauhan@users.noreply.github.com>
2023-07-01 00:07:01 +0530

    fixed prettier config mispell, add format script to package.json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,50 @@
-
+import { useState } from "react";
+import { searchForShows } from "../api/tvmaze";
 
 const Home = () => {
+  const [ searchStr, setSearchStr ] = useState('');
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null);
+
+  const onSearchInputChange = (ev) => {
+    setSearchStr(ev.target.value)
+  };
+
+  const onSearch = async (ev) => {
+    ev.preventDefault();
+
+    try{
+      const result = await searchForShows(searchStr);
+  setApiData(result);
+    } catch (error) {
+      setApiDataError(error);
+    }
+  };
+
+  const renderApiData = () => {
+    if (apiDataError) {
+      return <TextCenter>Error occured: {apiDataError.message}</TextCenter>;
+    }
+    
+    if (apiData) {
+      return apiData.map((data) => (
+        <div key={data.show.id}>{data.show.name}</div>
+      ))
+    }
+
+    return null
+  };
+
   return (
     <div>
-      Home
+      <form onSubmit={onSearch}>
+      <input type="text" value={searchStr} onChange={onSearchInputChange} />
+      <button type="submit">Search</button>
+      </form>
+
+      <div>{renderApiData()}</div>
     </div>
   )
-}
+};
 
 export default Home;
