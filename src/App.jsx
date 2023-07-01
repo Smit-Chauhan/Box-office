36de0ef1782bd10358074cd8f021c4db38ef9be6
devanshichauhan54 <98576336+Smit-Chauhan@users.noreply.github.com>
2023-07-02 01:30:59 +0530

    displayed dynamic show page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import MainLayout from './components/MainLayout';
+import Show from './pages/Show';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
           <Route path="/contact" element={<div>contact</div>} />
           <Route path="/Starred" element={<Starred />} />
         </Route>
+
+        <Route path="/show/:should" element={<show />} />
+
         <Route path="*" element={<div>Not found</div>} />
 
         {/*  <Route path="/" element={<App />}>
--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -18,7 +18,7 @@ const ShowCard = ({ name, image, id, summary }) => {
             <p>summary</p>
 
             <div>
-                <Link to="/">Read More</Link>
+                <Link to={`/show/${id}`}>Read More</Link>
                 <button type="button">Star me</button>
             </div>
         </div>
