05fbc59a385e2b5d2564dae497bc2f7511f1f9b0
devanshichauhan54 <98576336+Smit-Chauhan@users.noreply.github.com>
2023-07-02 23:35:45 +0530

    installed react-query. replaced useShowById with useQuery hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,31 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
-import MainLayout from './components/MainLayout';
 import Show from './pages/Show';
+import MainLayout from './components/MainLayout';
+import { GlobalTheme } from './theme';
 
-function App() {
+const queryClient = new QueryClient();
+
+  function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<div>contact</div>} />
-          <Route path="/Starred" element={<Starred />} />
-        </Route>
+    <QueryClientProvider client={queryClient}>
+      <GlobalTheme>
+        <HashRouter>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/starred" element={<Starred />} />
+            </Route>
 
-        <Route path="/show/:should" element={<show />} />
+            <Route path="/show/:showId" element={<Show />} />
 
-        <Route path="*" element={<div>Not found</div>} />
-
-        {/*  <Route path="/" element={<App />}>
-        <Route index element={<Home />} />
-        <Route path="teams" element={<Teams />}>
-          <Route path=":teamId" element={<Team />} />
-          <Route path="new" element={<NewTeamForm />} />
-          <Route index element={<LeagueStandings />} />
-        </Route>
-      </Route>
-      <Route element={<PageLayout />}>
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/tos" element={<Tos />} />
-      </Route>
-  <Route path="contact-us" element={<Contact />} /> */}
-      </Routes>
-    </BrowserRouter>
+            <Route path="*" element={<div>Not found</div>} />
+          </Routes>
+        </HashRouter>
+      </GlobalTheme>
+    </QueryClientProvider>
   );
 }
 
